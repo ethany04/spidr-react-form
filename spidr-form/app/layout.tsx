20a8fc5af6c8702295aa6f1ada3b3be8bfceb6f8
ethany04 <ethan.yu@utexas.edu>
2025-07-16 18:32:13 -0500

    update metadata

--- spidr-form/app/layout.tsx
+++ spidr-form/app/layout.tsx
@@ -1,4 +1,12 @@
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Air Fryer Interest Form",
+  icons: {
+    icon: "/spidr-logo.png", // or any other path if you change the file type
+  },
+};
 
 export default function RootLayout({
   children,

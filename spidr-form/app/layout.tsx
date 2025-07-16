81be85c3594f788c5aaa5224f3c4892e54ab9ddf
ethany04 <ethan.yu@utexas.edu>
2025-07-16 18:35:15 -0500

    update favicon

--- spidr-form/app/layout.tsx
+++ spidr-form/app/layout.tsx
@@ -3,9 +3,6 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Air Fryer Interest Form",
-  icons: {
-    icon: "/spidr-logo.png", // or any other path if you change the file type
-  },
 };
 
 export default function RootLayout({
